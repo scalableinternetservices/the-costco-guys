7d391e456197d1813c6fbb17e96847b3e5d2a735
mathiasUCSB <mathiasooi@ucsb.edu>
2024-11-14 17:17:50 -0800

    Add Session Controller Tests

--- test/controllers/session_controller_test.rb
+++ test/controllers/session_controller_test.rb
@@ -1,5 +1,9 @@
 require 'test_helper'
 
 class SessionControllerTest < ActionDispatch::IntegrationTest
-  
+  test "should get login form when not logged in" do
+    get login_path
+    assert_response :success
+    assert_template :login_form
+  end
 end
